75f567a120247f1878589e44b0fe3fa6d7079706
onepunchkim <hyukjin4711@gmail.com>
2025-04-23 18:24:16 +0900

    jdbc select

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -2,10 +2,15 @@ package hello.jdbc.repository;
 
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
@@ -15,6 +20,11 @@ class MemberRepositoryV0Test {
         //save
         Member member = new Member("memberV0", 10000);
         repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findById={}", findMember);
+        assertThat(findMember).isEqualTo(member);
     }
 
 }
